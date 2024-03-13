5abd7730f82489c51db09a51b572f5bb80c5802b
NewRecon <99maxwolf99@gmail.com>
2024-03-13 18:10:28 +0300

    Eggs

--- Game.cpp
+++ Game.cpp
@@ -1,4 +1,77 @@
 #include "Game.h"
+#include <Windows.h>
+
+Game::Game()
+	: window(sf::VideoMode(1200, 690), "Nu Pogodi!"),
+	th(incrementer)
+{
+	bgImage.loadFromFile("./image/bg.png");
+	bgShape.setSize(sf::Vector2f(1200, 690));
+	bgShape.setTexture(&bgImage);
+
+	screenImage.loadFromFile("./image/LU.png");
+	screenShape.setSize(sf::Vector2f(637, 410));
+	screenShape.setPosition(sf::Vector2f(280, 138));
+	screenShape.setTexture(&screenImage);
+
+	sf::Vector2f pos1(95, 440);
+	sf::Vector2f pos2(95, 565);
+	sf::Vector2f pos3(1045, 440);
+	sf::Vector2f pos4(1045, 565);
+
+	buttons.push_back(Button(pos1));
+	buttons.push_back(Button(pos2));
+	buttons.push_back(Button(pos3));
+	buttons.push_back(Button(pos4));
+
+	eggImage.loadFromFile("./image/egg.png");
+
+	int posEggs[24][3] = {  {330, 265, 20},
+							{370, 275, 80},
+							{400, 300, 140},
+							{420, 325, 180},
+							{440, 350, 220},
+							{450, 510, 200}, // ”пало
+
+							{330, 365, 20},
+							{370, 375, 80},
+							{400, 400, 140},
+							{420, 425, 180},
+							{440, 450, 220},
+							{450, 510, 200}, // ”пало
+
+							{870, 293, 200},
+							{854, 290, 140},
+							{820, 288, 80},
+							{780, 302, 20},
+							{745, 345, -40},
+							{765, 510, 200}, // ”пало
+
+							{870, 393, 200},
+							{854, 390, 140},
+							{820, 388, 80},
+							{780, 402, 20},
+							{745, 445, -40},
+							{765, 510, 200}, // ”пало
+	};
+
+	for (int i = 0; i < 24; i++) {
+		sf::RectangleShape shape;
+
+		shape.setSize(sf::Vector2f(26, 19));
+		shape.setTexture(&eggImage);
+		shape.setPosition(sf::Vector2f(posEggs[i][0], posEggs[i][1]));
+		shape.rotate(posEggs[i][2]);
+
+		eggShapes.push_back(shape);
+	}
+}
+
+
+Game::~Game() {
+	th.join();
+}
+
 
 void Game::processEvents() {
 	sf::Event event;
@@ -42,6 +115,7 @@ void Game::processEvents() {
 	}
 }
 
+
 void Game::update() {
 	switch (WolfPosition) {
 	case 0:
@@ -64,7 +138,18 @@ void Game::update() {
 		screenShape.setTexture(&screenImage);
 		break;
 	}
+
+	if (value % speed == 0) {
+		spawnCount++;
+		moveEggs();
+		if (spawnCount >= spawnRate) {
+			spawnCount = 0;
+			createRandomEgg();
+		}
+		//showInfoByEggs();
+	}
 }
+
 
 void Game::render() {
 	window.clear();
@@ -75,31 +160,56 @@ void Game::render() {
 		window.draw(el.getShape());
 	}
 
+	for (int i = 0, count = 0; i < 4; i++) {
+		for (int j = 0; j < 6; j++) {
+			if (eggStatus[i][j]) {
+				window.draw(eggShapes[count]);
+			}
+			count++;
+		}
+	}
+
+	/*for (auto egg : eggShapes) {
+		window.draw(egg);
+	}*/
+
 	window.display();
 }
 
-Game::Game()
-	: window(sf::VideoMode(1200, 690), "Nu Pogodi!")
-{
-	bgShape.setSize(sf::Vector2f(1200, 690));
-	bgImage.loadFromFile("./image/bg.png");
-	bgShape.setTexture(&bgImage);
 
-	screenImage.loadFromFile("./image/LU.png");
-	screenShape.setSize(sf::Vector2f(637, 410));
-	screenShape.setPosition(sf::Vector2f(280, 138));
-	screenShape.setTexture(&screenImage);
-
-	sf::Vector2f pos1(95, 440);
-	sf::Vector2f pos2(95, 565);
-	sf::Vector2f pos3(1045, 440);
-	sf::Vector2f pos4(1045, 565);
-
-	buttons.push_back(Button(pos1));
-	buttons.push_back(Button(pos2)); 
-	buttons.push_back(Button(pos3)); 
-	buttons.push_back(Button(pos4));
+void Game::createRandomEgg() {
+	eggStatus[rand() % 4][0] = true;
 }
+
+
+void Game::showInfoByEggs() {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 6; j++) {
+			cout << eggStatus[i][j] << " ";
+		}
+		cout << " : ";
+	}
+	cout << endl;
+}
+
+
+void Game::moveEggs() {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 6; j > 0; j--) {
+			swap(eggStatus[i][j], eggStatus[i][j - 1]);
+		}
+	}
+}
+
+
+void Game::incrementer() {
+	while (true) {
+		this_thread::sleep_for(chrono::milliseconds(100));
+		value++;
+		//cout << value << endl;
+	}
+}
+
 
 void Game::run() {
 	while (window.isOpen()) {
@@ -108,3 +218,5 @@ void Game::run() {
 		render();
 	}
 }
+
+int Game::value = 0;
--- Game.h
+++ Game.h
@@ -2,6 +2,9 @@
 #include <SFML/Graphics.hpp>
 #include "Button.h"
 #include <iostream>
+#include <thread>
+#include <chrono>
+using namespace std;
 
 class Game {
 private:
@@ -13,16 +16,36 @@ private:
 	sf::RectangleShape bgShape;
 	sf::Texture bgImage;
 
+	vector<sf::RectangleShape> eggShapes;
+	sf::Texture eggImage;
+
 	std::vector<Button> buttons;
 
-	int WolfPosition = 0; // 0 - LU, 1 - LD, 2 - RU, 3 - RD
+	int WolfPosition = 3; // 0 - LU, 1 - LD, 2 - RU, 3 - RD
+
+	int level = 1;
+	int speed = 10;
+	int spawnRate = 5;
+	int spawnCount = 0;
+
+	int mistakes = 0;
+	bool eggStatus[4][6]{};
+	int countCaughtEggs = 0;
+
+	static int value;
+	thread th;
+	void static incrementer();
 
 public:
 	Game();
+	~Game();
 	void run();
 
 private:
 	void processEvents();
 	void update();
 	void render();
+	void createRandomEgg();;
+	void moveEggs();
+	void showInfoByEggs();
 };
